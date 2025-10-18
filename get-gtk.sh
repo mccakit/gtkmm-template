28cdb611705f5b1a727b58bd2cc00ee107d25035
mccakit <mccakit09@gmail.com>
2025-10-18 20:13:38 +0300

    Initial commit

---
+++ get-gtk.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+export LD_LIBRARY_PATH=/home/mccakit/dev/dev-libs/gtk/native/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=/home/mccakit/dev/dev-libs/gtk/native/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH
+
+export LD_LIBRARY_PATH=/home/mccakit/dev/dev-libs/gtkmm/native/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=/home/mccakit/dev/dev-libs/gtkmm/native/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH
+
+export LD_LIBRARY_PATH=/home/mccakit/dev/dev-libs/icu/native/lib:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=/home/mccakit/dev/dev-libs/icu/native/lib/pkgconfig:$PKG_CONFIG_PATH
+
+export LD_LIBRARY_PATH=/home/mccakit/dev/dev-libs/xml2/native/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=/home/mccakit/dev/dev-libs/xml2/native/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH
+
+export LD_LIBRARY_PATH=/home/mccakit/dev/dev-libs/lerc/native/lib:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=/home/mccakit/dev/dev-libs/lerc/native/lib/pkgconfig:$PKG_CONFIG_PATH
+
+export LD_LIBRARY_PATH=/home/mccakit/dev/dev-libs/tiff/native/lib:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=/home/mccakit/dev/dev-libs/tiff/native/lib/pkgconfig:$PKG_CONFIG_PATH
+
+export LD_LIBRARY_PATH=/home/mccakit/dev/dev-libs/gstreamer/native/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=/home/mccakit/dev/dev-libs/gstreamer/native/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH
+
+export LD_LIBRARY_PATH=/home/mccakit/dev/dev-libs/rsvg/native/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=/home/mccakit/dev/dev-libs/rsvg/native/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH
